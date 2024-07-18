5afd3beb954d86a9effee119784d13779a045a2c
ThisIsMakena <james.a.cassidy@gmail.com>
2024-07-18 12:44:14 +1000

    Monsters can now be healed.

--- playerHealth.js
+++ playerHealth.js
@@ -6,8 +6,9 @@ async function applyHealing(entity, amount) {
             if (isNaN(amount)) {
                 throw new Error('Healing amount is not a number');
             }
-            entity.CurrentHealth = Math.min(entity.CurrentHealth + amount, entity.Health); // Use min to cap at max health
+            entity.CurrentHealth = Math.min(entity.CurrentHealth + amount, entity.hp); // Use min to cap at max health
             entity.CurrentHealth = parseInt(entity.CurrentHealth, 10); // Ensure CurrentHealth is an integer
+            console.log(entity.Health)
             resolve(entity); 
         } catch (error) {
             reject(error);
--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+//preload.js
+
 const { contextBridge, ipcRenderer } = require('electron');
 
 console.log('Preload script loaded');
@@ -18,38 +20,13 @@ contextBridge.exposeInMainWorld('api', {
     onInitPlayers: (callback) => {
         ipcRenderer.on('init-players', (event, players) => callback(players));
     },
-    applyHealing: (player, amount) => applyHealing(player, amount),
-    applyDamage: (player, amount) => applyDamage(player, amount),
+    applyHealing: (player, amount) => ipcRenderer.invoke('apply-healing', player, amount),
+    applyDamage: (player, amount) => ipcRenderer.invoke('apply-damage', player, amount),
     rollDie: (sides) => ipcRenderer.invoke('roll-die', sides),
     getMonsters: () => ipcRenderer.invoke('get-monsters'),
     computeAttackValue: (attackValue) => ipcRenderer.invoke('compute-attack-values', attackValue)
     
 });
-
-// Custom functions
-async function applyHealing(player, amount) {
-    return new Promise((resolve, reject) => {
-        try {
-            player.CurrentHealth = Math.min(player.CurrentHealth + amount, player.Health);
-            player.CurrentHealth = parseInt(player.CurrentHealth, 10); // Ensure CurrentHealth is an integer
-            resolve(player);
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-
-async function applyDamage(player, amount) {
-    return new Promise((resolve, reject) => {
-        try {
-            player.CurrentHealth = Math.max(player.CurrentHealth - amount, 0);
-            player.CurrentHealth = parseInt(player.CurrentHealth, 10); // Ensure CurrentHealth is an integer
-            resolve(player);
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
 
 console.log('contextBridge exposed in main world');
 
